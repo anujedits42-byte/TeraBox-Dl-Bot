baeb2ac0167c5f5e35a0f47ec5f5d1d6392bb5c8
•_• <abhinavaditya035@gmail.com>
2026-03-01 20:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN: str        = os.environ("BOT_TOKEN", "8598064539:AAFFDzeNlqPAOdhTvGIizQ9eL2Wp9Tf9jqQ")
-NDUS_COOKIE: str      = os.environ("NDUS_COOKIE")
+BOT_TOKEN: str        = os.environ.get("BOT_TOKEN", "8598064539:AAFFDzeNlqPAOdhTvGIizQ9eL2Wp9Tf9jqQ")
+NDUS_COOKIE: str      = os.environ.get("NDUS_COOKIE")
 
-API_ID: int | None    = int(os.environ["API_ID", "27322718"]) if os.getenv("API_ID") else None
+API_ID: int | None    = int(os.environ.get["API_ID", "27322718"]) if os.getenv("API_ID") else None
 API_HASH: str | None  = os.getenv("API_HASH", "4f6d1b67cf101aea5cf0536885aa1b82")
 SESSION_STRING: str | None = os.getenv("SESSION_STRING")
 
